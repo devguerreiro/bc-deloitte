b36e3f1570410b2ffc20c1638356f758b1935739
Luis Guerreiro <luiscvlh11@gmail.com>
2023-10-25 18:07:56 -0300

    feat: :tada: iniciado projeto em django

---
+++ gpapp/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for gpapp project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'gpapp.settings')
+
+application = get_wsgi_application()
