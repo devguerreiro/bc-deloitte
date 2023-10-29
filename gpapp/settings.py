53306b10b1e39a8cb68420c47b8872e9c6a1fb51
Luis Guerreiro <luiscvlh11@gmail.com>
2023-10-29 06:51:46 -0300

    feat: :sparkles: adicionado cors headers

--- gpapp/settings.py
+++ gpapp/settings.py
@@ -30,6 +30,10 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000",
+]
+
 
 # Application definition
 
@@ -40,9 +44,10 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "gpapp.core",
     "rest_framework",
     "rest_framework.authtoken",
+    "corsheaders",
+    "gpapp.core",
 ]
 
 REST_FRAMEWORK = {
@@ -58,6 +63,7 @@ REST_FRAMEWORK = {
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
+    "corsheaders.middleware.CorsMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
